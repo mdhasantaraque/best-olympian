8d899aacfabfc41fc9882c9b910a9d47b1d355ad
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-09-29 17:27:53 +0600

    sidebar updated

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./SideBar.css";
 
 const SideBar = ({ side }) => {
-  //   console.log(side);
-  //   let { sideBar } = props;
-  //   console.log(sideBar);
+  console.log(side);
+
   let name;
   let address;
   let details;
   let weight;
   let height;
   let age;
+  let timing = 0;
   for (const singlePlayer of side) {
     name = singlePlayer.name;
     address = singlePlayer.address;
@@ -18,6 +18,7 @@ const SideBar = ({ side }) => {
     weight = singlePlayer.weight;
     height = singlePlayer.height;
     age = singlePlayer.age;
+    timing = timing + singlePlayer.timing;
   }
 
   return (
@@ -27,24 +28,40 @@ const SideBar = ({ side }) => {
         <h3>Name: {name}</h3>
         <p>{address}</p>
       </div>
-
-      <div className="player-mesure">
-        <div>
-          <p>{weight}kg</p>
-          <p>Weight</p>
+      <div className="player-box">
+        <div className="player-measure">
+          <div>
+            <p>{weight}kg</p>
+            <p>Weight</p>
+          </div>
+          <div>
+            <p>{height}mtr</p>
+            <p>Height</p>
+          </div>
+          <div>
+            <p>{age}yrs</p>
+            <p>Age</p>
+          </div>
         </div>
-        <div>
-          <p>{height}mtr</p>
-          <p>Height</p>
-        </div>
-        <div>
-          <p>{age}yrs</p>
-          <p>Age</p>
-        </div>
+        <p>{details}</p>
       </div>
-      <p>{details}</p>
+
       <div>
         <h4>Add a Break</h4>
+        <div>
+          <button className="break-btn">10s</button>
+          <button className="break-btn">20s</button>
+          <button className="break-btn">30s</button>
+          <button className="break-btn">40s</button>
+        </div>
+        <div>
+          <h4>Timing Details</h4>
+          <h5 className="time-break">Timing Total {timing.toFixed(2)}</h5>
+          <h5 className="time-break">Break Time</h5>
+        </div>
+        <button className="activity-btn">
+          <p>Activity Completed</p>
+        </button>
       </div>
     </div>
   );
